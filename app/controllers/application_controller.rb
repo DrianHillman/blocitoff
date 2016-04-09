afd0e6eeab58dd005414b58e4b18bdd8977d1521
Drian Hillman <dhillman24@gmail.com>
2016-04-09 19:07:06 +0000

    Installed Devise for User Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+# This will require user sign-in to do anything with the app
+  # before_action :authenticate_user!
 end
